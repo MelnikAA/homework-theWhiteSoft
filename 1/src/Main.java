c6fbd810db903f701acb89bf6e3938f115de1036
MelnikAA <i89141800166@yandex.ru>
2022-05-11 21:25:03 +1000

    Refactoring

--- 1/src/Main.java
+++ 1/src/Main.java
@@ -4,24 +4,47 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static int clamp(int min, int max, int value){
+    public static int clamp(int min, int max, int value) {
         return Math.max(min, Math.min(max, value));
     }
 
+    public static int printPageUp(int currentRow, int rows, List<String> book) {
+        int toRow = clamp(0, book.size(), currentRow + rows);
+        for (int row = currentRow; row < toRow; ++row) {
+            System.out.println(book.get(row));
+        }
+        return currentRow;
+    }
+
+    public static int printPageDown(int currentRow, int rows, List<String> book) {
+        int toRow = clamp(0, book.size(), currentRow - rows);
+        for (int row = toRow; row < currentRow; ++row) {
+            System.out.println(book.get(row));
+        }
+        return currentRow;
+    }
+
+
     public static void main(String[] args) {
+
+        final int maxLength = 36;
+        final int rowsInPage = 10;
+
         System.out.println("Введите текст");
         Scanner in = new Scanner(System.in);
         String input = in.nextLine();
+
         List<String> book = new ArrayList<>();
+
         String[] words = input.trim().split("\\s+");
         StringBuilder line = new StringBuilder();
+
         int sumLine = 0;
         for (String word : words) {
-            if ((sumLine + word.length() > 36)) {
+            if ((sumLine + word.length() > maxLength)) {
                 book.add(line.toString());
                 sumLine = 0;
                 line = new StringBuilder();
-
             }
             line.append(word);
             line.append(" ");
@@ -30,33 +53,16 @@ public class Main {
 
         }
 
-        int now = 0;
-        int rows = 10;
-        int rowCount = book.size();
-        int border = clamp(0, rowCount, now + rows);
-        for (int row = now; row < border; ++row) {
-            System.out.println(book.get(row));
-        }
-
-        now = border;
+        int now = printPageUp(0, rowsInPage, book);
         while (true) {
-
-            String abc = in.nextLine();
+            input = in.nextLine();
             System.out.println();
-            switch (abc) {
+            switch (input) {
                 case "d" -> {
-                    border = clamp(0, rowCount, now + rows);
-                    for (int row = now; row < border; ++row) {
-                        System.out.println(book.get(row));
-                    }
-                    now = border;
+                    now = printPageUp(now, rowsInPage, book);
                 }
                 case "a" -> {
-                    border = clamp(0, rowCount, now - rows);
-                    for (int row = border; row < now; ++row) {
-                        System.out.println(book.get(row));
-                    }
-                    now = border;
+                    now = printPageDown(now, rowsInPage, book);
                 }
             }
         }
